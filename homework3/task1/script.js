93559b7326679ff6b6f2fc73538b85270c9df7c5
Anton Romashko <anton.romashko@pm.bet>
2020-07-08 15:21:03 +0300

    homework3: part1 complete

--- homework3/task1/script.js
+++ homework3/task1/script.js
@@ -1,68 +1,79 @@
-const lastValue = 30;
 const myProgressBar = document.getElementById('svg');
 const myTextItem = document.querySelector('text');
-let zeroOffset = 942;
+
+const defaultParams = {
+  startButton: 'Start',
+  pauseButton: 'Pause',
+  goOnButton: 'Go on',
+  zeroOffset: '942px'
+};
+
+// const lastValue = 0; // reverse mode
+// const firstValue = 30;
+
+const firstValue = 0; // normal mode
+const lastValue = 30;
+
+let minimalOffset = 942;
 let myInterval;
-let step = zeroOffset/lastValue;
+let step = minimalOffset / Math.max(lastValue, firstValue);
+
+myTextItem.innerHTML = firstValue.toString();
+
+function changeButtonTo(newButtonName, textVal, offset = false) {
+  let currentButton = document.getElementsByTagName('input')[1];
+  currentButton.id = newButtonName.toLowerCase();
+  currentButton.value = newButtonName;
+  myProgressBar.style.strokeDashoffset = defaultParams.zeroOffset;
+  document.querySelector('text').innerHTML = textVal;
+  if (offset) {
+    minimalOffset = 942;
+  }
+}
 
 function myTimer() {
-  let currentValue = myTextItem.innerHTML.split(':')[1];
-  if(currentValue.startsWith('0')) {
-    currentValue = currentValue[1]
-  }
-  let nextValue = parseInt(currentValue) + 1;
-  if (nextValue === lastValue) {
-    myTextItem.innerHTML = `00:${nextValue.toString()}`;
-    clearInterval(myInterval);
-  let pauseButton = document.getElementsByTagName('input')[1];
-  pauseButton.id = 'start';
-  pauseButton.value = 'Start';
-  myProgressBar.style.strokeDashoffset = '942px';
-document.querySelector('text').innerHTML = '00:00';
-  }
-  if (nextValue < 10) {
-    nextValue = `0${nextValue.toString()}`;
+  let currentValue = myTextItem.innerHTML;
+  let nextValue;
+  if (lastValue === 30) {
+    nextValue = parseInt(currentValue) + 1;
   }
   else {
-    nextValue = nextValue.toString();
+    nextValue = parseInt(currentValue) - 1;
   }
-  myTextItem.innerHTML = `00:${nextValue}`;
-  let currentOffset = zeroOffset;
-  zeroOffset = zeroOffset - step;
+  if (nextValue === lastValue) {
+    myTextItem.innerHTML = nextValue.toString();
+    clearInterval(myInterval);
+    changeButtonTo(defaultParams.startButton, firstValue);
+  }
+  myTextItem.innerHTML = nextValue.toString();
+  let currentOffset = minimalOffset;
+  minimalOffset = minimalOffset - step;
   myProgressBar.animate([
-    { strokeDashoffset: currentOffset.toString() },
-    { strokeDashoffset: zeroOffset.toString() }],
+      {strokeDashoffset: currentOffset.toString()},
+      {strokeDashoffset: minimalOffset.toString()}],
     {duration: 1000});
-  console.log(zeroOffset);
-  return true
 }
 
 function endTimer() {
   clearInterval(myInterval);
-  let pauseButton = document.getElementsByTagName('input')[1];
-  pauseButton.id = 'start';
-  pauseButton.value = 'Start';
-  myProgressBar.style.strokeDashoffset = '942px';
-  zeroOffset = 942;
-  document.querySelector('text').innerHTML = '00:00';
+  changeButtonTo(defaultParams.startButton, firstValue, true);
+  myTextItem.innerHTML = firstValue.toString();
 }
 
-function startTimer(param) {
-   if (param.id === 'pause') {
-     clearInterval(myInterval);
-     myProgressBar.style.strokeDashoffset = zeroOffset.toString();
-     param.value = 'Go on';
-     param.id = 'go-on';
-   }
-   else if (param.id === 'go-on') {
-     myInterval = setInterval(myTimer, 1000);
-     myProgressBar.style.strokeDashoffset = zeroOffset.toString();
-     param.value = 'Pause';
-     param.id = 'pause';
-   }
-   else {
-     param.value = 'Pause';
-     param.id = 'pause';
-     myInterval = setInterval(myTimer, 1000);
-   }
+function runPauseButton(param) {
+  if (param.id === defaultParams.pauseButton.toLowerCase()) {
+    clearInterval(myInterval);
+    myProgressBar.style.strokeDashoffset = minimalOffset.toString();
+    param.value = defaultParams.goOnButton;
+    param.id = defaultParams.goOnButton.toLowerCase().replace(' ', '-');
+  } else {
+    if (param.id === defaultParams.startButton.toLowerCase()) {
+      minimalOffset = 942;
+      myTextItem.innerHTML = firstValue.toString();
+    }
+    myInterval = setInterval(myTimer, 1000);
+    param.value = defaultParams.pauseButton;
+    param.id = defaultParams.pauseButton.toLowerCase();
+    myProgressBar.style.strokeDashoffset = minimalOffset.toString();
+  }
 }
