5b3fe78bdf01524f01808f6ad02903605d7c7d0f
Anton Romashko <anton.romashko@pm.bet>
2020-07-19 16:43:26 +0300

    hw4: task 1 with recursion

--- homework4/task1/script.js
+++ homework4/task1/script.js
@@ -2,28 +2,40 @@ const validateRules = {
   index: /^\d{5}$/,
   host: /^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/,
   money: /\$[\d]*\.?[\d]*/g,
-  date: /^(\0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[12])\/[\d]{4}$/
+  date: /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[12])\/[\d]{4}$/
 };
+const myInput = document.getElementById('validate');
+const allGroups = document.querySelectorAll('ul');
 
-function validateData() {
-  let allGroups = document.querySelectorAll('ul');
-  for (let i = 0; i <= allGroups.length - 1; i++) {
-    let currentChildren = allGroups[i].getElementsByTagName('li');
-    let pattern = validateRules[allGroups[i].id];
-    for (let j = 0; j <= currentChildren.length - 1; j++) {
-      let currentText = currentChildren[j].innerHTML.toString();
-      if (allGroups[i].id === 'money') {
-        let result = currentText.match(pattern);
-        for (let e = 0; e < result.length; e++) {
-          currentChildren[j].innerHTML = currentChildren[j].innerHTML.replace(result[e], `<span>${result[e]}</span>`);
-        }
+function validateData(val) {
+  if (val < 0) {
+    return;
+  } else {
+    let currentChildren = allGroups[val].getElementsByTagName('li');
+    let pattern = validateRules[allGroups[val].id];
+    function func(gr) {
+      if (gr < 0) {
+        return;
       } else {
-        if (validateRules[allGroups[i].id].test(currentText)) {
-          currentChildren[j].style.backgroundColor = 'lightgreen'
+        let currentText = currentChildren[gr].innerHTML.toString();
+        func(gr - 1)
+        if (allGroups[val].id === 'money') {
+          let result = currentText.match(pattern);
+          for (let e of result) {
+            currentChildren[gr].innerHTML = currentChildren[gr].innerHTML.replace(e, `<span>${e}</span>`);
+          }
         } else {
-          currentChildren[j].style.backgroundColor = 'orangered'
+          if (validateRules[allGroups[val].id].test(currentText)) {
+            currentChildren[gr].style.backgroundColor = 'lightgreen'
+          } else {
+            currentChildren[gr].style.backgroundColor = 'orangered'
+          }
         }
       }
     }
+    validateData(val - 1)
+    func(currentChildren.length - 1)
   }
 }
+
+myInput.addEventListener('click', (val) => { validateData(allGroups.length - 1) } );
