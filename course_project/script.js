2bab8695387e319cc5ee5923acbcf9f444a7d1de
Anton Romashko <anton.romashko@pm.bet>
2020-08-02 21:55:31 +0300

    coursework

--- course_project/script.js
+++ course_project/script.js
@@ -1,21 +1,35 @@
 async function getData() {
-  let response = await fetch('https://swapi.dev/api/people/8/');
+  let response = await fetch('https://swapi.dev/api/people/?page=1');
   let result = await response.json()
+  let pageCards = document.querySelectorAll('div.flip-card-front');
+  console.log(pageCards)
+  for (let i=0; i<result['results'].length; i++) {
+    pageCards[i].innerText = result['results'][i].name
+  }
   return result
 }
-
-// console.log(getData())
+window.addEventListener('load', getData)
 
 const main = document.querySelector('.main')
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+let clickedCard;
 
 function clickButton(event) {
+  console.log(event.target.parentElement)
   if (event.target.className === 'flip-card-front') {
     let parentElem = event.target.parentElement
-    parentElem.style.transform = 'rotateY(180deg)';
-    //parentElem.parentElement.style.transform = 'rotateY(180deg)';
-    parentElem.style.backgroundColor = 'transparent'
-    //parentElem.parentElement.style.backgroundColor = 'transparent'
+    clickedCard = parentElem
+    parentElem.classList.add('flipped')
+    modal.style.display = "block";
   }
 }
 
 main.addEventListener('click', clickButton)
+
+
+span.onclick = function() {
+  clickedCard.classList.remove('flipped')
+  modal.style.display = "none";
+
+}
