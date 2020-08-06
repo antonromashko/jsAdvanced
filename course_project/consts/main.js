5366b95f76616149149a7a68285db9911de6bdd6
Anton Romashko <anton.romashko@pm.bet>
2020-08-06 23:53:06 +0300

    coursework: constants

---
+++ course_project/consts/main.js
@@ -0,0 +1,11 @@
+export const STAR_WARS = {
+  MAIN_DIV: document.querySelector('.main'),
+  PERSONAGE_MODAL: document.getElementById("myModal"),
+  CLOSE_MODAL_BUTTON: document.querySelector(".close"),
+  BUTTONS: {
+    NEXT_BUTTON: document.querySelector('#next'),
+    PREVIOUS_BUTTON: document.querySelector('#previous')
+  },
+  PAGE_CARDS_DIV: document.querySelectorAll('div.flip-card-front'),
+  FIRST_PAGE_URL: 'https://swapi.dev/api/people/'
+}
